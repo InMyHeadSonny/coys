81c68538d86478c50f2c521e20b2eee1274ee75b
InMyHeadSonny <esters98@naver.com>
2023-10-19 15:36:29 +0900

    8개의 기본형을 알맞은 자리에 넣으시 => 클래스 변경

--- demo 3/src/main/java/practice2_1.java
+++ demo 3/src/main/java/practice2_1.java
@@ -1,6 +1,6 @@
-public class practice2 {
+public class practice2_1 {
     public static void main(String[] args) {
-        System.out.println("1byte : boolean 논리형"); /* 기본값 */
+         System.out.println("1byte : boolean 논리형"); /* 기본값 */
         System.out.println("2byte : char 문자형"); /* 기본값 */
         System.out.println("1byte : byte 정수형");
         System.out.println("2byte : short 정수형");
