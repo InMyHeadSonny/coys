4ddf2b6e80c958c0637fa56e76fd9110956e4a0f
InMyHeadSonny <esters98@naver.com>
2023-10-19 17:50:23 +0900

    키워드 예약단어의 출력값 그리고 데이터 타입에따른 결과값

---
+++ demo 3/src/main/java/practice2_7.java
@@ -0,0 +1,11 @@
+public class practice2_7 {
+    /* 문장들의 출력결과 */
+    public static void main(String[] args){
+        System.out.println("1" + "2");/* String + String = "3" */
+        /*System.out.println(ture + "");    /* 키워드예약단어 이므로 다른 단어로 변수선언후 사용해야한다. "Ture" */
+        System.out.println('A' + 'B'); /* 문자 + 문자 = 아스키코드로 출력된다 */
+        System.out.println('1' + 2); /* '1' = 49 아스키 문자 + 2숫자   49 +2 =51
+        System.out.println('J' + "ava");
+        /* System.out.println(ture + null);  키워드예약단어 + 키워드예약단어 각 올바른변수를 선언해야한다. */
+    }
+}
