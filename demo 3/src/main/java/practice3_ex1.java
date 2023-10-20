84c8014574fbb79fce19231826934555f8dbc6ce
InMyHeadSonny <esters98@naver.com>
2023-10-20 10:01:49 +0900

    연산자의 결합규칙 (우변의 값을 좌변에 저장)

---
+++ demo 3/src/main/java/practice3_ex1.java
@@ -0,0 +1,9 @@
+public class practice3_ex1 {
+    public static void main(String [] args) {
+        int x, y;
+
+        x = y = 3; /* y 에 3이 저장된 후에 x에 3이 저장된다. */
+        System.out.println("x=" + x);
+        System.out.println("y=" + y);
+    }
+}
