3f5d66198d55c46f012a754fb3681dc0ce1f8347
InMyHeadSonny <esters98@naver.com>
2023-10-20 13:00:38 +0900

    기존값을 변환 공식을 사용하여 다른값을 도출하는방법

---
+++ demo 3/src/main/java/practice3_6.java
@@ -0,0 +1,10 @@
+public class practice3_6 {
+    public static void main(String[] args) {
+        int fahrenheit = 100;
+        float celcuis = (fahrenheit - 32) * 5/9;
+
+        System.out.println("Fahrenheit:" + fahrenheit);
+        System.out.println("Celcius" + celcuis); /* C = (F - 32) * 5/9 */
+
+    }
+}
