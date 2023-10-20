362bd2458d651e60a889f1b49e16acae65858427
InMyHeadSonny <esters98@naver.com>
2023-10-20 12:54:59 +0900

    조건연산자를 두번사용하여 양수 정수 0을 파악하는 코드

---
+++ demo 3/src/main/java/practice3_5.java
@@ -0,0 +1,7 @@
+public class practice3_5 {
+    public static void main(String[] args) {
+        int num = 10;
+        String result = (num > 0) ? "양수" : (num < 0) ? "음수" : "0";
+        System.out.println(result);
+    }
+}
