07c15eb298c7a46b1f20c2dc0cf327a19a184a67
InMyHeadSonny <esters98@naver.com>
2023-11-17 18:53:29 +0900

    배열의 출력 예제5-8 2차원 배열의 초기화 예제2 오류수정2

--- demo 3/src/main/java/javapractice/arrays_8.java
+++ demo 3/src/main/java/javapractice/arrays_8.java
@@ -11,8 +11,8 @@ public class arrays_8 {
         };
         // 과목별 총점
         int korTotal = 0, engTotal = 0, mathTotal = 0;
-        System.out.println("번호 국어 영어 수학 총점 평균 ");
-        System.out.println("=======================");
+        System.out.println("번호  국어  영어  수학  총점  평균 ");
+        System.out.println("===============================");
 
         for(int i=0;i < score.length;i++) {
             int sum = 0; // 개인별 총점
@@ -25,11 +25,11 @@ public class arrays_8 {
 
             for (int j = 0; j < score[i].length; j++) {
                 sum += score[i][j];
-                System.out.printf("%5d %5.1f%n", sum, avg);
+                System.out.printf("%5d",score[i][j]);
 
             }
 
-            avg = sum / (float) score[i].length; //평균계산
+            avg = sum /(float) score[i].length; //평균계산
             System.out.printf("%5d %5.1f%n", sum, avg);
         }
 
