afe11cafc8d5e51120639c5f5bee1e5e881667a9
InMyHeadSonny <esters98@naver.com>
2023-11-20 14:56:30 +0900

    배열 연습문제 2차원 배열 arr에 담긴 모든 값의 총합과 평균을 구하는 프로그램을 완성하시오 문제해결 arr.length x,y *

--- demo 3/src/main/java/javapractice/array_practice3.java
+++ demo 3/src/main/java/javapractice/array_practice3.java
@@ -3,10 +3,10 @@ package javapractice;
 public class array_practice3 {
     public static void main(String[] args) {
         int[][] arr = {
-                { 5, 5, 5, 5, 5 },
-                { 10, 10, 10, 10, 10 },
-                { 20, 20, 20, 20, 20 },
-                { 30, 30, 30, 30, 30 }
+                {5, 5, 5, 5, 5},
+                {10, 10, 10, 10, 10},
+                {20, 20, 20, 20, 20},
+                {30, 30, 30, 30, 30}
         };
 
         int total = 0;
@@ -14,17 +14,18 @@ public class array_practice3 {
 
         // 알맞은 코드를 넣어 완성하시오
 
-        for (int i =0; i<arr.length;i++){
-            for (int j= 0; j <arr[i].length; j++)
+        for (int i = 0; i < arr.length; i++) {
+            int xyCount = 0;
+            for (int j = 0; j < arr[i].length; j++)
                 total += arr[i][j];
+            xyCount = arr.length * arr[i].length;
+            average = total / (float) xyCount;
+
+
         }
-        average = total / (float)arr.length ;
+            // 알맞은 코드를 넣어 완성하시오
+            System.out.println("total=" + total);
+            System.out.println("average=" + average);
 
-
-
-        // 알맞은 코드를 넣어 완성하시오
-
-        System.out.println("total=" + total);
-        System.out.println("average=" + average);
     }
 }
