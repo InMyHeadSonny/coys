6f5d4fd3a7ca31b42c96c264425164f77ddd23b9
InMyHeadSonny <esters98@naver.com>
2023-11-20 15:33:05 +0900

    배열 연습문제 1과9사이의 중복되지 않은 숫자로 이루어진 3자리 숫자를 만들어내는 프로그램

--- demo 3/src/main/java/javapractice/array_practice4.java
+++ demo 3/src/main/java/javapractice/array_practice4.java
@@ -10,15 +10,18 @@ public class array_practice4 {
             int j = (int) (Math.random() * ballArr.length);
             int tmp = 0;
             // 알맞은 코드를 넣어 완성하시오
-
+            tmp = ballArr[i];
+            ballArr[i] = ballArr[j];
+            ballArr[j] = tmp;
 
             // 알맞은 코드를 넣어 완성하시오
 
         }
 
         //배열 ballArr의 앞에서 3개의 수를 배열 ball3로 복사한다.
-
-
+        for (int i = 0; i < ball3.length; i++) {
+            ball3[i] = ballArr[i];
+        }
         //배열 ballArr의 앞에서 3개의 수를 배열 ball3로 복사한다.
         for(int i=0;i<ball3.length;i++) {
             System.out.println(ball3[i]);
