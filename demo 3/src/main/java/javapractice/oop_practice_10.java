2dae1575cf2e8e1937977130b8fbdc61c0859934
InMyHeadSonny <esters98@naver.com>
2023-11-27 14:58:49 +0900

    oop 예제 6-11  기본생성자 compile error 예제 2

--- demo 3/src/main/java/javapractice/oop_practice_10.java
+++ demo 3/src/main/java/javapractice/oop_practice_10.java
@@ -14,7 +14,7 @@ class Data_2 {
 }
 public class oop_practice_10 {
     public static void main(String[] args) {
-        Data_1 = new Data_1();
-        Data_2 = new Data_2(); //compile error 발생
+        Data_1 d1 = new Data_1();
+        Data_2 d2 = new Data_2(10); //compile error 발생
     }
 }
