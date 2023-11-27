1a923f8f1bfb02df630922718cad06af4a4d2216
InMyHeadSonny <esters98@naver.com>
2023-11-27 14:57:12 +0900

    oop 예제 6-11  기본생성자 compile error 예제

---
+++ demo 3/src/main/java/javapractice/oop_practice_10.java
@@ -0,0 +1,20 @@
+package javapractice;
+
+class Data_1 {
+    int value;
+}
+
+class Data_2 {
+    int value;
+
+    Data_2(int x) { // 매개변수가 있는 생성자.
+        value = x;
+
+    }
+}
+public class oop_practice_10 {
+    public static void main(String[] args) {
+        Data_1 = new Data_1();
+        Data_2 = new Data_2(); //compile error 발생
+    }
+}
