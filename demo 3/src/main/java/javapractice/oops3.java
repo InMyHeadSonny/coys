7a90453ea3546eb8b1c6a0daa48e1b478d3f4d62
InMyHeadSonny <esters98@naver.com>
2023-12-05 16:00:16 +0900

    oop part2 참조변수 super 예제2

---
+++ demo 3/src/main/java/javapractice/oops3.java
@@ -0,0 +1,24 @@
+package javapractice;
+
+class Ex7_3 {
+    public static void main(String[] args) {
+        Child2 c = new Child2();
+        c.method();
+    }
+}
+
+class Parent2 { int x = 10; /*super.x와 this.x 둘 다 가능 */ }
+
+class Child2 extends Parent2 {
+    void method() {
+
+        System.out.println("x=" + x);
+        System.out.println("this.x=" + this.x);
+        System.out.println("super.x=" + super.x);
+    }
+}
+
+//멤버가같다.
+
+public class oops3 {
+}
