04af6f2fb21e600deb6066788a61ed70b65133ad
InMyHeadSonny <esters98@naver.com>
2023-12-06 12:47:27 +0900

    oop2 anonymous class 예제

---
+++ demo 3/src/main/java/javapractice/oops15.java
@@ -0,0 +1,39 @@
+package javapractice;
+
+import jdk.jfr.Event;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.beans.EventHandler;
+
+class Ex7_18 {
+    public static void main(String[] args) {
+        Button b = new Button("Start");
+        b.addActionListener(new EventHandler());
+    }
+}
+
+class EventHandler implements ActionListener {
+    public void actionPerformed(ActionEvent e) {
+        System.out.println("ActionEvent occurred!!!");
+    }
+}
+
+
+
+class Ex7_19 {
+    public static void main(String[] args) {
+        Button c = new Button("Start");
+        c.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("ActionEvent occurred!!!");
+            }
+        }
+
+        );
+    }
+}
+
+public class oops15 {
+}
