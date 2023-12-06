3a1c4686a7b806a642f572b71e060b1e0f89fae4
InMyHeadSonny <esters98@naver.com>
2023-12-06 16:28:51 +0900

    oop2 연습문제5

--- demo 3/src/main/java/oop_practice3.java
+++ demo 3/src/main/java/oop_practice3.java
@@ -1,44 +1,47 @@
 class MyTv {
-    private boolean IsPowerOn;
-    private int channel;
+    private int currentChannel;
+    private int prevChannel; // 추가: 이전 채널을 저장하는 변수
     private int volume;
 
     final int MAX_VOLUME = 100;
     final int MIN_VOLUME = 0;
     final int MAX_CHANNEL = 100;
-    final int MIN_CHANNEL = 1; // 수정: MIN_CHANNEL 이름 변경
+    final int MIN_CHANNEL = 1;
 
+    public int getCurrentChannel() {
+        return currentChannel;
+    }
 
-
-    // 수정: 채널 및 볼륨을 설정하는 메서드 추가
-    void setChannel(int newChannel) {
+    public void setCurrentChannel(int newChannel) {
         if (newChannel >= MIN_CHANNEL && newChannel <= MAX_CHANNEL) {
-            channel = newChannel;
+            currentChannel = newChannel;
         }
     }
 
-    int getChannel() {
-        return channel;
+    public int getVolume() {
+        return volume;
     }
 
-    void setVolume(int newVolume) {
+    public void setVolume(int newVolume) {
         if (newVolume >= MIN_VOLUME && newVolume <= MAX_VOLUME) {
             volume = newVolume;
         }
     }
 
-    int getVolume() {
-        return volume;
+    // 이전 채널로 이동하는 메서드
+    public void gotoPrevChannel() {
+        if (prevChannel != 0) { // 이전 채널이 기록되어 있을 때만 적용
+            int temp = currentChannel;
+            currentChannel = prevChannel;
+            prevChannel = temp;
+        }
     }
-}
 
-public class oop_practice3 {
-    public static void main(String[] args) {
-        MyTv t = new MyTv();
-
-        t.setChannel(10);
-        System.out.println("CH: " + t.getChannel()); // 수정: println 문 오류 수정
-        t.setVolume(20);
-        System.out.println("VOL: " + t.getVolume()); // 수정: println 문 오류 수정
+    // 채널 변경 시, 이전 채널을 갱신하는 메서드
+    public void setChannel(int newChannel) {
+        if (newChannel >= MIN_CHANNEL && newChannel <= MAX_CHANNEL) {
+            prevChannel = currentChannel;
+            currentChannel = newChannel;
+        }
     }
 }
